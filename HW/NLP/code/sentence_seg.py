059c8c32e73508676ea423cc8f94de2addee9e2c
lllssf <lsf0614@163.com>
2019-06-28 15:22:41 +0800

    Update sentence_seg.py

--- HW/NLP/code/sentence_seg.py
+++ HW/NLP/code/sentence_seg.py
@@ -1,21 +1,16 @@
 # 分句: ‘’“”【】[]''中的内容不能分割
-text = '1.玄德幼时，与乡中小儿戏于树下，曰：“我为天子，当乘此车盖。”2.行吧，【唉呀妈呀，好的吧】'
-
 def sentence_seg(para):
     sentence = []
-    quote  = re.sub(r'(.*[，。！ ])(.*[‘|“|【|\[|\(|\'].*[’|”|】|\]|\'])', r'\1\n\2',para,re.M)
-    quote = quote.split()
+    quote  = re.split(r'(\w*：?“.*”|\w*:?".*"|\w*【.*】)',para,re.M) # 将带引用符号的子句拆分，本数据集中只有这三种引用符
     for q in quote:
-        if "“" not in q:
-            q = re.sub('([，！？\?  ])([^”’])', r"\1\n\2",q,re.M)
-            q = re.sub('(.*)(\d.*)',r"\1\n\2",q,re.M)
+        f = re.search(r'[‘|“|【|\[|\(|\'|\"]',q) # 对不带引用符号的句子进一步拆分
+        if  f == None:
+            q = re.sub('([，。！？ ])(.*)', r"\1\n\2",q,re.M) # 按中文标点拆分
+            q = re.sub('(.*)(\d.*)',r"\1\n\2",q,re.M) # 将形如“1.balabala”的句子拆开
             q = q.split('\n')
             for s in q:
-                sentence.append(s)
+                if s != '':
+                    sentence.append(s)
         else:
             sentence.append(q)
     return sentence
-
-
-s = sentence_seg(text)
-print('\n'.join(s))

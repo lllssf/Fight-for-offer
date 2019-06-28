a00c75b35f61b7df386b0047ac726d954aae2274
lllssf <lsf0614@163.com>
2019-06-28 15:31:47 +0800

    Update sentence_seg.py

--- HW/NLP/code/sentence_seg.py
+++ HW/NLP/code/sentence_seg.py
@@ -6,7 +6,7 @@ def sentence_seg(para):
         f = re.search(r'[‘|“|【|\[|\(|\'|\"]',q) # 对不带引用符号的句子进一步拆分
         if  f == None:
             q = re.sub('([，。！？ ])(.*)', r"\1\n\2",q,re.M) # 按中文标点拆分
-            q = re.sub('(.*)(\d.*)',r"\1\n\2",q,re.M) # 将形如“1.balabala”的句子拆开
+            q = re.sub('(.*)(\d\..*)',r"\1\n\2",q,re.M) # 将形如“1.balabala”的句子拆开
             q = q.split('\n')
             for s in q:
                 if s != '':
